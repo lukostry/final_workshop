088190ff8f5ebce93368df5111a7c11e8387683d
lukostry <lukasz.ostrowski85@gmail.com>
2016-04-21 15:36:42 +0200

    Added sticky menu

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
+    
+    //Adding event listener on internal anchors to enable smooth scrolling
 
     $("a.internal").click(function(e) {
-        console.log("działa");
         e.preventDefault();
+        
         var dest = $(this).attr("href");
+        
+        history.pushState(null, null, dest);
         
         //The following function will animate click on an internal anchor within document
         
@@ -14,4 +18,34 @@ $(document).ready(function() {
         }
         scrollToAnchor();
     });
+    
+    
+    //Creating sticky menu
+    
+    var navigationBar = $("nav");
+    var menuPosition = navigationBar.offset().top;
+    var scrollPosition = $(document).scrollTop();
+    
+    function checkCurrentPosition() {
+        
+        if (scrollPosition > menuPosition) {
+            navigationBar.addClass("sticky");
+//            console.log("Pojechaliśmy za daleko");
+        } else {
+//            console.log("Wciąż za blisko");
+            navigationBar.removeClass("sticky");
+        }
+    }
+    checkCurrentPosition();
+    
+    $(window).on("scroll", function() {
+        scrollPosition = $(document).scrollTop();
+//        menuPosition = $(navigationBar).offset().top;
+        console.log(scrollPosition);
+        console.log(menuPosition);
+        checkCurrentPosition();    
+    });
+    
+    
+    
 });    
