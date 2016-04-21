599d596ccfde140dc62239422e8e85f36ee74980
lukostry <lukasz.ostrowski85@gmail.com>
2016-04-22 00:15:49 +0200

    An infinite slider created, added new images

--- js/app.js
+++ js/app.js
@@ -40,12 +40,80 @@ $(document).ready(function() {
     
     $(window).on("scroll", function() {
         scrollPosition = $(document).scrollTop();
-//        menuPosition = $(navigationBar).offset().top;
-        console.log(scrollPosition);
-        console.log(menuPosition);
+
+        /*console.log(scrollPosition);
+        console.log(menuPosition);*/
+        
         checkCurrentPosition();    
     });
     
+    //Creating slider in our_team section
     
-    
+    $(".slide_viewer").each(function() {              // Dla każdej grupy slajdów.
+        
+        var $group  = $(".slide_group");        // Pobranie elementu o klasie slide-group (kontener).
+        var $slides = $group.find(".slide");       // Obiekt jQuery przechowujący wszystkie slajdy.
+        var currentIndex = 0;                     // Numer indeksu bieżącego slajdu.
+        var nextSlide = $(".next_slide");      //definiuje zmienne przechowujące przyciski
+        var prevSlide = $(".prev_slide");
+        
+        function move(newIndex) {          // Przejście ze starego do nowego slajdu.
+            var animateLeft, slideLeft;      // Deklaracja zmiennych.
+        
+        /*
+            // Jeżeli odtwarzana jest animacja slajdu, to nie podejmujemy żadnych działań.
+            if ($group.is(':animated')) {  
+              return;
+            }
+        */
+        
+            if (newIndex > currentIndex) {   // Jeżeli nowy element > bieżący.
+                slideLeft = "100%";            // Umieszczenie nowego slajdu po prawej stronie.
+                animateLeft = "-100%";         // Animacja bieżącej grupy w lewą stronę.
+            } else {                         // W przeciwnym razie.
+                slideLeft = '-100%';           // Umieszczenie nowego slajdu po lewej stronie.
+                animateLeft = '100%';          // Animacja bieżącej grupy w prawą stronę.
+            }
+            // Umieszczenie nowego slajdu po lewej (jeśli ma mniejszą wartość indeksu niż bieżący) lub prawej (jeśli ma tę wartość większą).
+            $slides.eq(newIndex).css( {left: slideLeft, display: "block"} );
+        
+            $group.animate({
+                left: animateLeft
+            }, 500, function() {    // Animacja slajdu
+                    $slides.eq(currentIndex).css( {display: 'none'} ); // i ukrycie poprzedniego.     
+                    $slides.eq(newIndex).css( {left: 0} ); // Ustawienie położenia dla nowego slajdu.
+                    $group.css( {left: 0} );               // Ustawienie położenia grupy slajdów.
+                    currentIndex = newIndex;               // Ustawienie zmiennej currentIndex wartości nowego obrazu.
+            });
+        }
+        
+        function moveToNextSlide() {                     // Przejście do następnego slajdu
+            
+              if (currentIndex < ($slides.length - 1)) { // Jeżeli to nie jest ostatni slajd.
+                  move(currentIndex + 1);            // Przejście do następnego slajdu.
+              } else {                             // W przeciwnym razie.
+                  move(0);                           // Przejście do pierwszego slajdu.
+              }
+        }
+        
+        function moveToPrevSlide() {                     // Przejście do poprzedniego slajdu
+            
+              if (currentIndex !== 0) { // Jeżeli to nie jest pierwszy slajd.
+                  move(currentIndex - 1);            // Przejście do poprzedniego slajdu.
+              } else {                             // W przeciwnym razie.
+                  move($slides.length - 1);                           // Przejście do pierwszego slajdu.
+              }
+        }
+        
+        nextSlide.on("click", function(e) {
+            e.preventDefault();
+            moveToNextSlide ();
+        });
+        
+        prevSlide.on("click", function(e) {
+            e.preventDefault();
+            moveToPrevSlide();
+        });
+                  
+      });
 });    
